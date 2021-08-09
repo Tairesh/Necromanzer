aaf7f09cfc06d827154c78272fb654a7f9aab701
Ilya Agafonov <tairesh.rus@gmail.com>
2021-08-09 21:33:41 +0500

    validate settings

--- src/settings.rs
+++ src/settings.rs
@@ -30,11 +30,12 @@ impl Settings {
 
     pub fn load() -> Result<Settings, String> {
         let path = Path::new(PATH);
-        let settings: Settings;
+        let mut settings: Settings;
         if path.is_file() {
             let file = File::open(path).unwrap();
             let reader = BufReader::new(file);
             settings = serde_json::from_reader(reader).unwrap();
+            settings.validate();
         } else {
             settings = Settings::default()?;
             serde_json::to_writer(&File::create(path).unwrap(), &settings).unwrap();
@@ -44,6 +45,26 @@ impl Settings {
     }
 
     pub fn save(&mut self) {
+        self.validate();
         serde_json::to_writer(&File::create(Path::new(PATH)).unwrap(), self).unwrap();
+    }
+
+    fn validate(&mut self) -> &Settings {
+        if self.width < 640 {
+            self.width = 640;
+        }
+        if self.width > 1920 {
+            self.width = 1920;
+        }
+        if self.height < 480 {
+            self.height = 480;
+        }
+        if self.height > 1280 {
+            self.height = 1280;
+        }
+        if self.music_volume > 128 {
+            self.music_volume = 128;
+        }
+        self
     }
 }
